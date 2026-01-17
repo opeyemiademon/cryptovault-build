ade8f489fad01af8df2d86f79333f769e19a0447
Opeyemi Mansoor <opeyemiademon@gmail.com>
2026-01-17 22:05:59 +0100

    vercel added

--- utils/constants.d.ts
+++ utils/constants.d.ts
@@ -1,5 +1,5 @@
 export declare const TOKEN_SECRET: string;
 export declare const TOKEN_EXPIRY = "7d";
-export declare const MONGO_URL: string;
+export declare const MONGO_URL = "mongodb+srv://kayus4school_db_user:kBNxfhBTYvB5qgJI@cluster0.kbid8zc.mongodb.net/cryptovault?retryWrites=true&w=majority&appName=Cluster0";
 export declare const PORT: number;
 //# sourceMappingURL=constants.d.ts.map
--- utils/constants.js
+++ utils/constants.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const TOKEN_SECRET = process.env.JWT_SECRET || 'cryptovault-secret-key-change-in-production';
 export const TOKEN_EXPIRY = '7d';
-export const MONGO_URL = process.env.MONGO_URL || process.env.DATABASE_URL || 'mongodb://localhost:27017/cryptovault';
+export const MONGO_URL = 'mongodb+srv://kayus4school_db_user:kBNxfhBTYvB5qgJI@cluster0.kbid8zc.mongodb.net/cryptovault?retryWrites=true&w=majority&appName=Cluster0';
 export const PORT = parseInt(process.env.PORT || '9000', 10);
 //# sourceMappingURL=constants.js.map
